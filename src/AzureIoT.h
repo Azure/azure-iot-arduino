4ab57aea22f4e787ffe7ddb8ad03e7a57adc6628
Sandeep Mistry <s.mistry@arduino.cc>
2015-12-04 14:59:18 -0500

    Add license headers

--- src/AzureIoT.h
+++ src/AzureIoT.h
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 #include "sdk/lock.h"
 #include "sdk/threadapi.h"
 #include "sdk/serializer.h"
