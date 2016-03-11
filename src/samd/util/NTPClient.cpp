db42b07e06b754c455c55a0bdb941fd783986006
Andre Price <obsoleted@users.noreply.github.com>
2016-03-11 13:03:28 -0800

    Single HttpsClient implementation for all arch
     - Avoid code duplciation by combining the arch specific httpsclient
     implementations and using #ifdef-ing for unique bits (as they are only
             in includes and class definition now
     - Similarly for samd use a common NTPClient header for both adafruit
     and arduino boards

--- src/samd/util/NTPClient.cpp
+++ src/samd/util/NTPClient.cpp
@@ -1,11 +1,8 @@
 // Copyright (c) Arduino. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 #if defined(ARDUINO_ARCH_SAMD)
-#if defined(ARDUINO_SAMD_FEATHER_M0)
-#include "../featherm0/util/NTPClient.h"
-#else
 #include "NTPClient.h"
-#endif
 
 #define LOCAL_UDP_PORT 2390
 
--- src/util/HTTPSClient.h
+++ src/util/HTTPSClient.h
@@ -2,14 +2,26 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 //
 
+#ifdef ARDUINO_SAMD_FEATHER_M0
 #include <Adafruit_WINC1500SSLClient.h>
+#elif defined(ARDUINO_SAMD_ZERO) || defined(ARDUINO_SAMD_MKR1000)
+#include <WifiSSLClient.h>
+#elif defined(ESP8266)
+#include <WiFiClientSecure.h>
+#endif
 
 #ifndef HTTPSCLIENT_H
 #define HTTPSCLIENT_H
 
 #define HTTPS_PORT          443
 
+#ifdef ARDUINO_SAMD_FEATHER_M0
 class HTTPSClient : public Adafruit_WINC1500SSLClient
+#elif defined(ARDUINO_SAMD_ZERO) || defined(ARDUINO_SAMD_MKR1000)
+class HTTPSClient : public WifiSSLClient
+#elif defined(ESP8266)
+class HTTPSClient : public WiFiClientSecure
+#endif
 {
     public:
         HTTPSClient();
